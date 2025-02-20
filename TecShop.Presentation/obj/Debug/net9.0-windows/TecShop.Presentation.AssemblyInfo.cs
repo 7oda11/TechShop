f9596bcf62b567e761f29c6cb754ef5217870d9c
AbuHussien28 <aliabuhussien@outlook.com>
2025-02-20 09:43:37 +0200

    Make WelcomeScreen Ui

--- TecShop.Presentation/obj/Debug/net9.0-windows/TecShop.Presentation.AssemblyInfo.cs
+++ TecShop.Presentation/obj/Debug/net9.0-windows/TecShop.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TecShop.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5eeb8fe4e43ded0099c2df57273127a6c831fd16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f773e10888b36b710d69f868fab4de4fa94e6a48")]
 [assembly: System.Reflection.AssemblyProductAttribute("TecShop.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TecShop.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
